94f21a40a1499fc9f529ecd5050ad7b8b0821d4f
Laurence Welch <lwelch1@ithaca.edu>
2013-10-24 16:47:53 -0400

    Started on Eblearn
    Classes Added:
    svector
    state_forwardvector

---
+++ Classes/Misc/svector.vb
@@ -0,0 +1,107 @@
+﻿Public Class svector(Of T)
+    Implements IList(Of T), IDisposable
+
+
+    Protected Friend L As New List(Of T)
+    Public Sub New()
+        L = New List(Of T)
+
+    End Sub
+
+    Public Sub New(n As Integer)
+        L = New List(Of T)(n)
+    End Sub
+
+    Public Sub Add(item As T) Implements System.Collections.Generic.ICollection(Of T).Add
+        L.Add(item)
+    End Sub
+
+    Public Overridable Sub Clear() Implements System.Collections.Generic.ICollection(Of T).Clear
+        L.Clear()
+    End Sub
+
+    Public Function Contains(item As T) As Boolean Implements System.Collections.Generic.ICollection(Of T).Contains
+        Return L.Contains(item)
+    End Function
+
+    Public Sub CopyTo(array() As T, arrayIndex As Integer) Implements System.Collections.Generic.ICollection(Of T).CopyTo
+        L.CopyTo(array, arrayIndex)
+    End Sub
+
+    Public ReadOnly Property Count As Integer Implements System.Collections.Generic.ICollection(Of T).Count
+        Get
+            Return L.Count
+        End Get
+    End Property
+
+    Public ReadOnly Property IsReadOnly As Boolean Implements System.Collections.Generic.ICollection(Of T).IsReadOnly
+        Get
+            Return False
+        End Get
+    End Property
+
+    Public Function Remove(item As T) As Boolean Implements System.Collections.Generic.ICollection(Of T).Remove
+        L.Remove(item)
+    End Function
+
+    Public Function GetEnumerator() As System.Collections.Generic.IEnumerator(Of T) Implements System.Collections.Generic.IEnumerable(Of T).GetEnumerator
+        Return L.GetEnumerator
+    End Function
+
+    Public Function IndexOf(item As T) As Integer Implements System.Collections.Generic.IList(Of T).IndexOf
+        Return L.IndexOf(item)
+    End Function
+
+    Public Sub Insert(index As Integer, item As T) Implements System.Collections.Generic.IList(Of T).Insert
+        L.Insert(index, item)
+    End Sub
+
+    Default Public Property Item(index As Integer) As T Implements System.Collections.Generic.IList(Of T).Item
+        Get
+            Return L.Item(index)
+        End Get
+        Set(value As T)
+            L.Item(index) = value
+        End Set
+    End Property
+
+    Public Sub RemoveAt(index As Integer) Implements System.Collections.Generic.IList(Of T).RemoveAt
+        L.RemoveAt(index)
+    End Sub
+
+    Public Function GetEnumerator1() As System.Collections.IEnumerator Implements System.Collections.IEnumerable.GetEnumerator
+        Return GetEnumerator()
+    End Function
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not Me.disposedValue Then
+            If disposing Then
+                L = Nothing
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        Me.disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(ByVal disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
+End Class
---
+++ NetLearn/State/state_forwardvector.vb
@@ -0,0 +1,37 @@
+﻿Imports Netdx
+Public Class state_forwardvector(Of T)
+    Inherits svector(Of T)
+    Dim parent As T
+
+    Sub New()
+        MyBase.new()
+        parent = Nothing
+    End Sub
+
+    Sub New(other As state_forwardvector(Of T))
+        Me.L = other.L
+    End Sub
+
+    Sub New(n As Integer)
+        MyBase.New(n)
+        parent = Nothing
+    End Sub
+
+    Overrides Sub Clear()
+        If Not parent Is Nothing Then
+            For i = 1 To Me.Count - 1
+                RemoveAt(i)
+            Next
+        Else
+            For i = 0 To Me.Count - 1
+                RemoveAt(i)
+            Next
+        End If
+        
+    End Sub
+
+    Shadows Sub Clear(n As Integer)
+        RemoveAt(n)
+    End Sub
+
+End Class
